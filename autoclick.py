94937140818af6ec5913f03c83f2be145ced045b
Edgar Young <edgaryoung@MacBook-Pro-3.local>
2026-01-15 16:42:07 -0600

    adding autoclick file

--- autoclick.py
+++ autoclick.py
@@ -12,15 +12,14 @@ try:
     while True:
         pyautogui.click()
 
-        # move mouse slightly at random (-10 to +10 pixels)
-        dx = random.randint(-10, 10)
-        dy = random.randint(-10, 10)
+        # move mouse slightly at random (-20 to +20 pixels)
+        dx = random.randint(-20, 20)
+        dy = random.randint(-20, 20)
         pyautogui.moveRel(dx, dy)
 
         time.sleep(1)# wait 1 second between clicks
         #reset mouse position to center of screen
         screenWidth, screenHeight = pyautogui.size()
         pyautogui.moveTo(screenWidth / 2, screenHeight / 2)
-        
 except pyautogui.FailSafeException:
     print("Stopped safely.")
